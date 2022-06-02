216dc94b30a922a84d4f426e032397e7737f5511
shubham0204 <equipintelligence@gmail.com>
2022-06-02 21:21:20 +0530

    Added onScreenStatus to exported report

--- app/src/main/java/com/shubham0204/ocmsdashboard/MainActivity.kt
+++ app/src/main/java/com/shubham0204/ocmsdashboard/MainActivity.kt
@@ -1,59 +1,92 @@
 package com.shubham0204.ocmsdashboard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DownloadManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.FirebaseDatabase
 import com.shubham0204.ocmsdashboard.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var activityMainBinding : ActivityMainBinding
-    private val ON_SCREEN_STATUS = "on_screen_status"
-    private val ON_SCREEN_APP = "on_screen_app"
-    private val CAMERA_PERMISSION_STATUS = "camera_status"
-    private val AUDIO_PERMISSION_STATUS = "audio_status"
-    private val USAGE_STATS_PERMISSION_STATUS = "usage_status"
-    private val PRESENCE_STATUS = "presence"
-
+    private lateinit var studentStatsAdapter: StudentStatsAdapter
+    private lateinit var reportString : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate( layoutInflater )
         setContentView( activityMainBinding.root )
 
-        /*val studentStatusRef = FirebaseDatabase.getInstance().reference.child( "shubham_panchal" )
-        studentStatusRef.addValueEventListener( object : ValueEventListener {
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val status = snapshot.child( ON_SCREEN_STATUS ).getValue( Boolean::class.java )
-                val app = snapshot.child( ON_SCREEN_APP ).getValue( String::class.java )
-                val cameraPermission = snapshot.child( CAMERA_PERMISSION_STATUS ).getValue( Boolean::class.java )
-                val audioPermission = snapshot.child( AUDIO_PERMISSION_STATUS ).getValue( Boolean::class.java )
-                val usagePermission = snapshot.child( USAGE_STATS_PERMISSION_STATUS ).getValue( Boolean::class.java )
-                val presenceStatus = snapshot.child( PRESENCE_STATUS ).getValue( String::class.java )
-                activityMainBinding.textView.text =
-                    "User on screen: $status \nApp on screen: $app \n" +
-                            "Camera permission: $cameraPermission \nAudio permission: $audioPermission \n" +
-                            "Usage access permission: $usagePermission \nPresence: $presenceStatus"
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })*/
-
         val dataRepository = StudentStatsRepository( FirebaseDatabase.getInstance().reference )
-        val studentStatsAdapter = StudentStatsAdapter( this , dataRepository.studentStatsFlow )
+        studentStatsAdapter = StudentStatsAdapter(
+            this ,
+            dataRepository.studentStatsFlow ,
+            studentStatLongClickListener
+        )
         activityMainBinding.recyclerview.layoutManager = LinearLayoutManager( this )
         activityMainBinding.recyclerview.adapter = studentStatsAdapter
 
+        activityMainBinding.downloadReportButton.setOnClickListener{ downloadReport() }
+        activityMainBinding.downloadReportButton.setOnLongClickListener{ _ ->
+            studentStatsAdapter.clearAll()
+            FirebaseDatabase.getInstance().reference.removeValue().addOnSuccessListener {
+                Toast.makeText( this@MainActivity , "Database cleared." , Toast.LENGTH_LONG ).show()
+            }
+            true
+        }
+
+    }
+
+    private fun downloadReport() {
+        val reportBuilder = StringBuilder()
+        studentStatsAdapter.report.forEach{
+            reportBuilder.append( it.first + " " + it.second + "\n" )
+        }
+        reportString = reportBuilder.toString()
+        openCreateFileDialog()
+    }
+
+    private fun openCreateFileDialog() {
+        // https://developer.android.com/training/data-storage/shared/documents-files#create-file
+        val intent = Intent( Intent.ACTION_CREATE_DOCUMENT ).apply {
+            addCategory( Intent.CATEGORY_OPENABLE )
+            type = "text/plain"
+            putExtra( Intent.EXTRA_TITLE , "report.txt" )
+        }
+        createFileIntentLauncher.launch( intent )
+    }
+
+    private val createFileIntentLauncher = registerForActivityResult( ActivityResultContracts.StartActivityForResult() ) {
+        val fileUri = it.data?.data
+        if ( fileUri != null ) {
+            try {
+                val outputStream = contentResolver.openOutputStream( fileUri )
+                outputStream?.write( reportString.toByteArray() )
+                outputStream?.close()
+            }
+            catch ( e : FileNotFoundException ) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private val studentStatLongClickListener = object : StudentStatsAdapter.StudentStatsInteractCallback {
+
+        override fun onStudentStatLongClickListener(studentStats: StudentStats) {
+
+        }
 
     }
 
--- app/src/main/java/com/shubham0204/ocmsdashboard/StudentStatsAdapter.kt
+++ app/src/main/java/com/shubham0204/ocmsdashboard/StudentStatsAdapter.kt
@@ -2,22 +2,35 @@ package com.shubham0204.ocmsdashboard
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
-import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.LinkedHashMap
 
 class StudentStatsAdapter(
     private var context: Context ,
-    private var studentStatsFlow : Flow<StudentStats>
+    private var studentStatsFlow : Flow<StudentStats> ,
+    private var longClickListener : StudentStatsInteractCallback
     ) : RecyclerView.Adapter<StudentStatsAdapter.StudentStatsViewHolder>() {
 
+    private val dateFormat = SimpleDateFormat( "kk:mm dd-MM-yyyy" , Locale.getDefault() )
     private val studentStatsList = ArrayList<StudentStats>()
+    val report = ArrayList<Pair<String,String>>()
+
+    interface StudentStatsInteractCallback {
+        fun onStudentStatLongClickListener( studentStats: StudentStats )
+    }
 
     init {
         CoroutineScope( Dispatchers.Main ).launch {
@@ -28,16 +41,9 @@ class StudentStatsAdapter(
                 }
                 else {
                     val position = studentStatsList.indexOf( it )
-                    when ( it.isActive ) {
-                        true -> {
-                            studentStatsList[ position ] = it
-                            notifyItemChanged( position )
-                        }
-                        false -> {
-                            studentStatsList.removeAt( position )
-                            notifyItemRemoved( position )
-                        }
-                    }
+                    compare( studentStatsList[ position ] , it )
+                    studentStatsList[ position ] = it
+                    notifyItemChanged( position )
                 }
             }
         }
@@ -53,18 +59,60 @@ class StudentStatsAdapter(
         holder.studentName.text = studentStat.name
         holder.studentAppName.text = studentStat.onScreenApp
         holder.studentPresence.text = studentStat.presenceStatus
+        if ( !studentStat.isActive ) {
+            holder.studentStatsItemView.background = ColorDrawable( Color.RED )
+            arrayOf( holder.studentAppName , holder.studentPresence , holder.studentPresence ).forEach {
+                it.setTextColor( Color.WHITE )
+            }
+        }
+        else {
+            holder.studentStatsItemView.background = ColorDrawable( Color.WHITE )
+        }
+        holder.studentStatsItemView.setOnLongClickListener {
+            longClickListener.onStudentStatLongClickListener( studentStat )
+            true
+        }
     }
 
     override fun getItemCount(): Int {
         return studentStatsList.size
     }
 
+    fun clearAll() {
+        studentStatsList.clear()
+        notifyItemRangeChanged( 0 , 0 )
+        notifyDataSetChanged()
+    }
+
+    private fun compare( stats1 : StudentStats , stats2 : StudentStats ) {
+        if ( stats1.presenceStatus != stats2.presenceStatus ) {
+            var message = ""
+            message = if ( stats1.presenceStatus == "Present" ) {
+                "is in the meeting"
+            } else {
+                "is not in the meeting"
+            }
+            report.add( Pair( getCurrentTime() , "${stats2.name} $message"))
+        }
+        if ( stats1.onScreenStatus != stats2.onScreenStatus ) {
+            var message = ""
+            message = if ( stats1.onScreenStatus ) {
+                "is using the meeting app"
+            } else {
+                "switched to some other app"
+            }
+            report.add( Pair( getCurrentTime() , "${stats2.name} $message"))
+        }
+    }
+
+    private fun getCurrentTime() : String = dateFormat.format( Date() )
 
     class StudentStatsViewHolder( itemView : View ) : RecyclerView.ViewHolder( itemView ) {
 
         var studentName : TextView = itemView.findViewById( R.id.student_stats_name )
         var studentAppName : TextView = itemView.findViewById( R.id.student_stats_app_on_screen )
         var studentPresence : TextView = itemView.findViewById( R.id.student_stats_presence )
+        var studentStatsItemView : ConstraintLayout = itemView.findViewById( R.id.student_stats_constraint_layout )
 
     }
 
